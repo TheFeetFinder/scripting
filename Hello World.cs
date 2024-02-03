033674b9828c7b4e44638d86daa1aa639a2d800c
TheFeetFinder <158765160+TheFeetFinder@users.noreply.github.com>
2024-02-03 18:25:12 +0100

    Add files via upload

---
+++ Hello World.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //outpot an stuf
+        print("i said hello world when i came out of yo mama last night"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //balls be bussin but i love me some of 'da bussy'
+
+}
+   
